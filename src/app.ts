77c106abc3e3d6c85fdc5340ae801f75a6ca8c21
Kurniadi Ahmad Wijaya <kurniadiahmadwijaya@gmail.com>
2024-05-15 22:40:49 +0700

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (_, res) => {
+    res.send("Server Running Properly")
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
